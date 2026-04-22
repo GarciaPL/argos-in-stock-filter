252ba73bf34f756a876d011747ac5988e9ba8901
Lukasz Ciesluk <lukaszciesluk@gmail.com>
2026-04-22 19:48:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
   #!/bin/bash
   # build.sh chrome  OR  build.sh firefox
   TARGET=$1
+  mkdir -p dist
   cp manifest.$TARGET.json manifest.json
   zip -r dist/argos-in-stock-filter-$TARGET.zip . -x "*.git*" "manifest.*.json" "build.sh" "dist/*"
